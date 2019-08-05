433e9ba87f004d322ed1b97c1f47d6159c0351e0
rjena <erzhenatasha9@yandex.ru>
2019-08-05 17:57:02 +0700

    readById & saveById of JSON done (?)

--- src/ru/eltex/app/java/lab5/ManagerOrderJSON.java
+++ src/ru/eltex/app/java/lab5/ManagerOrderJSON.java
@@ -1,10 +1,17 @@
 package ru.eltex.app.java.lab5;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import ru.eltex.app.java.lab3.Order;
 import ru.eltex.app.java.lab3.Orders;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class ManagerOrderJSON extends AManageOrder {
@@ -17,20 +24,54 @@ public class ManagerOrderJSON extends AManageOrder {
 
     @Override
     public Order readById(UUID id) {
+        if (file.exists() && file.length() != 0) {
+            JsonReader jr = null;
+            try {
+                Gson gson = new GsonBuilder().create();
+                jr = new JsonReader(new FileReader(file));
+                jr.setLenient(true);
+                Order order;
+                while (jr.peek() != JsonToken.END_DOCUMENT) {
+                    order = gson.fromJson(jr, Order.class);
+                    if (order.getId().equals(id)) return order;
+                }
+            } catch (IOException e) { e.printStackTrace();
+            } finally { if (jr != null) try { jr.close(); } catch (IOException e) { e.printStackTrace(); } }
+        }
         return null;
     }
 
     @Override
     public void saveById(Order order) {
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         /**
          * есть заказ с указанным id -> удалить и добавить заказ
          * нет заказа с указанным id -> добавить заказ
          */
+        try {
+            ArrayList<Order> orders = new ArrayList<>();
+            if (file.exists()) {
+                if (file.length() != 0) {
+                    JsonReader jr = null;
+                    try {
+                        Gson gson = new GsonBuilder().create();
+                        jr = new JsonReader(new FileReader(file));
+                        jr.setLenient(true);
+                        Order o;
+                        while (jr.peek() != JsonToken.END_DOCUMENT) {
+                            o = gson.fromJson(jr, Order.class);
+                            if (!o.getId().equals(order.getId())) orders.add(o);
+                        }
+                    } finally { if (jr != null) try { jr.close(); } catch (IOException e) { e.printStackTrace(); } }
+                }
+            } else file.createNewFile();
+            FileWriter fw = null;
+            try {
+                fw = new FileWriter(file);
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                for (Order o : orders) fw.write(gson.toJson(o) + "\n");
+                fw.write(gson.toJson(order) + "\n");
+            } finally { if (fw != null) try { fw.close(); } catch (IOException e) { e.getMessage(); } }
+        } catch (IOException e) { e.getMessage(); }
     }
 
     @Override
@@ -40,10 +81,7 @@ public class ManagerOrderJSON extends AManageOrder {
 
     @Override
     public void saveAll(Orders<Order> orders) {
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
+
 }
