358f3ca1656d9444a8e4e017d8ccc3ba6514c25c
rjena <erzhenatasha9@yandex.ru>
2019-08-28 15:42:36 +0700

    lab 8 (auto orders create when start)

--- src/main/java/ru/eltex/app/lab8/ServiceDB.java
+++ src/main/java/ru/eltex/app/lab8/ServiceDB.java
@@ -1,14 +1,17 @@
 package ru.eltex.app.lab8;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import ru.eltex.app.lab1.Electronic;
 import ru.eltex.app.lab1.Phone;
 import ru.eltex.app.lab1.Smartphone;
 import ru.eltex.app.lab1.Tablet;
+import ru.eltex.app.lab2.Credentials;
 import ru.eltex.app.lab3.Order;
 import ru.eltex.app.lab3.Orders;
 import ru.eltex.app.lab3.ShoppingCart;
+import ru.eltex.app.lab8.repositories.CredentialsRepo;
 import ru.eltex.app.lab8.repositories.ElectronicRepo;
 import ru.eltex.app.lab8.repositories.OrderRepo;
 import ru.eltex.app.lab8.repositories.ShoppingCartRepo;
@@ -23,6 +26,7 @@ public class ServiceDB {
     @Autowired private ElectronicRepo electronicRepo;
     @Autowired private ShoppingCartRepo shoppingCartRepo;
     @Autowired private OrderRepo orderRepo;
+    @Autowired private CredentialsRepo credentialsRepo;
 
     public Order readById(String id) {
         try { return orderRepo.findById(UUID.fromString(id)).get(); }
@@ -62,6 +66,34 @@ public class ServiceDB {
             electronicRepo.saveAndFlush(e);
             return e.getID().toString();
         } catch (Exception ex) { ex.getMessage(); }
+        return null;
+    }
+
+    @Bean
+    public String addOrders() {
+        int count = 0;
+        while (count < 3) {
+            Credentials cred = new Credentials("Surname" + ++count, "Name" + count, "Patronym" + count,
+                    "email" + count + "@gmail.com");
+            ShoppingCart<Electronic> cart = new ShoppingCart<>(count);
+            Electronic[] electronics = new Electronic[new Random().nextInt(3) + 1];
+            for (int j = 0; j < electronics.length; j++) {
+                Electronic e;
+                int t = new Random().nextInt(3);
+                if (t == 1) e = new Phone();
+                else if (t == 2) e = new Smartphone();
+                else e = new Tablet();
+                e.create();
+                e.setCart(cart);
+                electronics[j] = e;
+            }
+            Order order = new Order(cred, cart);
+            credentialsRepo.saveAndFlush(cred);
+            shoppingCartRepo.saveAndFlush(cart);
+            for (int j = 0; j < electronics.length; j++)
+                electronicRepo.saveAndFlush(electronics[j]);
+            orderRepo.saveAndFlush(order);
+        }
         return null;
     }
 }
